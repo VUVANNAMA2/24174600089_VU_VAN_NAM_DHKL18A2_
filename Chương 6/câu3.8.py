4338ed8b1f8f463be1b956fab3745b70fc6419eb
VUVANNAMA2 <vvnam.24174600089@sv.uneti.edu.vn>
2024-10-30 23:26:40 +0700

    câu3.8.py

---
+++ Chương 6/câu3.8.py
@@ -0,0 +1,15 @@
+#câu3
+# Nhập ba số từ người dùng
+a = float(input("Nhập số thứ nhất: "))
+b = float(input("Nhập số thứ hai: "))
+c = float(input("Nhập số thứ ba: "))
+
+# Tìm số lớn nhất
+if a >= b and a >= c:
+    số_lớn_nhất = a
+elif b >= a and b >= c:
+    số_lớn_nhất = b
+else:
+    số_lớn_nhất = c
+
+print(f"Số lớn nhất là: {số_lớn_nhất}")
