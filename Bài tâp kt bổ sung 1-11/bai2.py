13b7e5f019b966d85e4b7d277495ebe4ded3451e
VUVANNAMA2 <vvnam.24174600089@sv.uneti.edu.vn>
2024-11-05 14:41:16 +0700

    bai2.py

---
+++ Bài tâp kt bổ sung 1-11/bai2.py
@@ -0,0 +1,27 @@
+# Tính tổng 
+tong_s1 = 0
+n = int(input("nhap vao gia tri nguyen duong :"))
+for i in range(1, n+1):
+  tong_s1 = tong_s1 + i
+  if i > 0:
+   print(f"tong cac so tu nhien : {tong_s1}")
+# Tính tích  
+tich_s2=1
+n = int(input("nhap vao gia tri nguyen duong :"))
+for i in range(1, n-1):
+  if i >0:
+   tich_s2 = tich_s2 * i
+  print(f"tich cac so tu nhien : {tich_s2}")
+#Tính chuỗi 
+s3 = 0
+n = int(input("nhap cac so nguyen duong"))
+for i in range(1, n + 1):
+  s3 += (-1)**(i + 1) / i
+
+print(f"Tổng S3 đến n = {n} là: {s3}")
+#Tính S4
+s4 = 0
+n = int(input("nhap gia tri nguyen duong"))
+for k in range(n):
+  s_4 = s4 + k/(k+2)
+  print(f"Tổng S4 đến k = {n} là: {s4}")
