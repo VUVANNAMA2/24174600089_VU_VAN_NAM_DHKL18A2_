fe736219258527c865ff1662e65121d5a7085cb2
VUVANNAMA2 <vvnam.24174600089@sv.uneti.edu.vn>
2024-11-20 22:47:27 +0700

    bai2.py

---
+++ Bài tập luyện tập chuỗi ký tự/bai2.py
@@ -0,0 +1,5 @@
+# Nhập chuỗi từ người dùng
+chuoi_nhap_vao = input("nhap vao chuoi ky tu:")
+tach_chuoi=chuoi_nhap_vao.split() #Tách chuỗi thành danh sách các phần tử(split)
+chuoi_ket_qua = " ".join(tach_chuoi) #Nối các phần tử trong danh sách thành một chuỗi(join)
+print(f"chuoi ket qua: {chuoi_ket_qua}")
