d734b398cfc77ded52789a86532927134df1bbbc
VUVANNAMA2 <vvnam.24174600089@sv.uneti.edu.vn>
2024-11-21 21:51:12 +0700

    bai6.py

--- Bài tập luyện tập chuỗi ký tự/bai6.py
+++ Bài tập luyện tập chuỗi ký tự/bai6.py
@@ -4,10 +4,3 @@ if nhap_vao_chuoi.startswith('-') and chuoi_nhap[1:].isdigit(): #starswith lện
 else:
     print(" chuoi khong phai so am")
 
-#bài 7  Viết chương trình nhập vào chuỗi ký tự, kiểm tra xem chuỗi đó có phải số thập phân hay không
-nhap_vao_chuoi = input("nhap vao ky tu:")
-if nhap_vao_chuoi.isdecimal() and  nhap_vao_chuoi.isdigit():    #isdecimal kiểm tra xem số thập phân hay không isdigit kiểm tra phải là só hay k
-             print(nhap_vao_chuoi.isdecimal())
-             print("chuoi la so thap phan") #true
-else:
-         print(" chuoi la so thap phan")     #false     
