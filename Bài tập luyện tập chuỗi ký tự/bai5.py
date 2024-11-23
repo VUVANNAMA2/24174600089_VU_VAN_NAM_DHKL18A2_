4d19dbec0f4ca8eae18e188df7be6dfcb0f24986
VUVANNAMA2 <vvnam.24174600089@sv.uneti.edu.vn>
2024-11-23 20:32:33 +0700

    bai5.py

--- Bài tập luyện tập chuỗi ký tự/bai5.py
+++ Bài tập luyện tập chuỗi ký tự/bai5.py
@@ -1,9 +1,10 @@
+nhap_vao_chuoi = input("nhap vao chuoi ky tu")
 viet_hoa=0
 viet_thuong=0
-for chu in chuoi_nhap:
+for chu in nhap_vao_chuoi:
     if chu.isupper():# isupper kiểm tra chữ hoa
        viet_hoa=viet_hoa+1
     elif chu.islower():#islower kiểm tra chữ cái in thường
         viet_thuong=viet_thuong+1
 print(f"chu cai in hoa: {viet_hoa}")  
-print(f"chu cai in thuong: {viet_thuong}") 
+print(f"chu cai in thuong: {viet_thuong}")   
