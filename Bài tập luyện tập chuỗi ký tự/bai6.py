d230a1a74badcc0f2a97683a50d0063c7a1a8051
VUVANNAMA2 <vvnam.24174600089@sv.uneti.edu.vn>
2024-11-23 20:29:04 +0700

    bai6.py

--- Bài tập luyện tập chuỗi ký tự/bai6.py
+++ Bài tập luyện tập chuỗi ký tự/bai6.py
@@ -1,6 +1,5 @@
 nhap_vao_chuoi = input("nhap vao ky tu:")
-if nhap_vao_chuoi.startswith('-') and chuoi_nhap[1:].isdigit(): #starswith lệnh kiểm tra số gì "-"(la số âm)   isdigit kiểm tra xem là số hay k
+if nhap_vao_chuoi.startswith('-') and nhap_vao_chuoi[1:].isdigit(): #starswith lệnh kiểm tra số gì "-"(la số âm)   isdigit kiểm tra xem là số hay k
     print(" chuoi la so am")
 else:
     print(" chuoi khong phai so am")
-
